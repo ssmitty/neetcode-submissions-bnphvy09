98e1096cc73efae8b038c876f7a08bffc021f76a
ssmitty <159193435+ssmitty@users.noreply.github.com>
2026-04-10 19:45:08 -0400

    Add: daily-temperatures - submission-4

---
+++ Data Structures & Algorithms/daily-temperatures/submission-4.py
@@ -0,0 +1,20 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        res=[0]*(len(temperatures))
+        stack=[]
+        r=0
+        for o,i in enumerate(temperatures):
+            if len(stack)>0:
+                while len(stack)!=0 and i>stack[-1][1] :
+                    oind,ovalue= stack.pop()
+                    print(oind,ovalue)
+                    res[oind]= o-oind
+            stack.append((o,i))
+            r=o
+        l=len(temperatures)-1
+        while len(stack)!=0:
+            oind,ovalue=stack.pop() 
+            res[oind]= 0
+        return res
+        #hold tuple of index of where originally in array and subtract by i. Array will always be decreasing
+    
